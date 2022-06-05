12ac8b1605c3e85b5da27a49acbbf63edc10a6fc
Hunter <fffarmer@protonmail.com>
2022-06-05 12:56:46 -0400

    hopefully fix for templates

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -66,7 +66,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(PROJECT_DIR, "templates"),
+            os.path.join("templates/"),
         ],
         "APP_DIRS": True,
         "OPTIONS": {
